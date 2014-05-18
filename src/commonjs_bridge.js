747abba2a0d8e9b479ecfec24b0ee11d91e762e4
Egor Smirnov <egorsmir@gmail.com>
2014-05-18 15:52:26 +0200

    style: switch to usage of indentation with "tab" character

--- src/commonjs_bridge.js
+++ src/commonjs_bridge.js
@@ -2,105 +2,105 @@ var cachedModules = {};
 
 // load all modules
 for (var modulePath in window.__cjs_module__) {
-    require(modulePath, modulePath);
+	require(modulePath, modulePath);
 }
 
 function require(requiringFile, dependency) {
 
-    if (window.__cjs_module__ === undefined) throw new Error("Could not find any modules. Did you remember to set 'preprocessors' in your Karma config?");
-    if (window.__cjs_modules_root__ === undefined) throw new Error("Could not find CommonJS module root path. Please report this issue to the karma-commonjs project.");
+	if (window.__cjs_module__ === undefined) throw new Error("Could not find any modules. Did you remember to set 'preprocessors' in your Karma config?");
+	if (window.__cjs_modules_root__ === undefined) throw new Error("Could not find CommonJS module root path. Please report this issue to the karma-commonjs project.");
 
-    dependency = normalizePath(requiringFile, dependency, window.__cjs_modules_root__ || '');
+	dependency = normalizePath(requiringFile, dependency, window.__cjs_modules_root__ || '');
 
-    // find module
-    var moduleFn = window.__cjs_module__[dependency];
+	// find module
+	var moduleFn = window.__cjs_module__[dependency];
 
-    if (moduleFn === undefined) {
-        var possibleDependencies = [
-            dependency.split('.')[0] + '/Index.js',
-            dependency.split('.')[0] + '/index.js',
-            dependency.split('.')[0] + '/Index.coffee',
-            dependency.split('.')[0] + '/index.coffee'
-        ];
+	if (moduleFn === undefined) {
+		var possibleDependencies = [
+			dependency.split('.')[0] + '/Index.js',
+			dependency.split('.')[0] + '/index.js',
+			dependency.split('.')[0] + '/Index.coffee',
+			dependency.split('.')[0] + '/index.coffee'
+		];
 
-        for (var i = 0; i < possibleDependencies.length; i++) {
+		for (var i = 0; i < possibleDependencies.length; i++) {
 
-          dependency = possibleDependencies[i];
-          moduleFn = window.__cjs_module__[dependency];
+			dependency = possibleDependencies[i];
+			moduleFn = window.__cjs_module__[dependency];
 
-          if (typeof moduleFn !== 'undefined') {
-              break;
-          }
-        }
+			if (typeof moduleFn !== 'undefined') {
+				break;
+			}
+		}
 
-        if (moduleFn === undefined) {
-          throw new Error("Could not find module '" + dependency + "' from '" + requiringFile + "'");
-        }
-    }
+		if (moduleFn === undefined) {
+			throw new Error("Could not find module '" + dependency + "' from '" + requiringFile + "'");
+		}
+	}
 
-    // run the module (if necessary)
-    var module = cachedModules[dependency];
-    if (module === undefined) {
-        module = { exports: {} };
-        cachedModules[dependency] = module;
-        moduleFn(requireFn(dependency), module, module.exports);
-    }
-    return module.exports;
+	// run the module (if necessary)
+	var module = cachedModules[dependency];
+	if (module === undefined) {
+		module = { exports: {} };
+		cachedModules[dependency] = module;
+		moduleFn(requireFn(dependency), module, module.exports);
+	}
+	return module.exports;
 }
 
 function requireFn(basepath) {
-    return function(dependency) {
-        return require(basepath, dependency);
-    };
+	return function (dependency) {
+		return require(basepath, dependency);
+	};
 }
 
 function normalizePath(basePath, relativePath, modulesRoot) {
 
-    if (isFullPath(relativePath)) return relativePath;
-    if (isNpmModulePath(relativePath)) basePath = modulesRoot;
-    if (!isFullPath(basePath)) throw new Error("basePath should be full path, but was [" + basePath + "]");
+	if (isFullPath(relativePath)) return relativePath;
+	if (isNpmModulePath(relativePath)) basePath = modulesRoot;
+	if (!isFullPath(basePath)) throw new Error("basePath should be full path, but was [" + basePath + "]");
 
-    var baseComponents = basePath.split("/");
-    var relativeComponents = relativePath.split("/");
-    var nextComponent;
-    
-    if (!isNpmModulePath(relativePath)) {
-        baseComponents.pop();     // remove file portion of basePath before starting
-    }
-    while (relativeComponents.length > 0) {
-        nextComponent = relativeComponents.shift();
+	var baseComponents = basePath.split("/");
+	var relativeComponents = relativePath.split("/");
+	var nextComponent;
 
-        if (nextComponent === ".") continue;
-        else if (nextComponent === "..") baseComponents.pop();
-        else baseComponents.push(nextComponent);
-    }
-    
-    var normalizedPath = baseComponents.join("/");
+	if (!isNpmModulePath(relativePath)) {
+		baseComponents.pop();     // remove file portion of basePath before starting
+	}
+	while (relativeComponents.length > 0) {
+		nextComponent = relativeComponents.shift();
 
-    if (normalizedPath.substr(normalizedPath.length - 3) !== ".js") {
-        if (isCoffeeScript(basePath)) {
-            normalizedPath += ".coffee";
-        }
-        else {
-            normalizedPath += ".js";
-        }
-    }
+		if (nextComponent === ".") continue;
+		else if (nextComponent === "..") baseComponents.pop();
+		else baseComponents.push(nextComponent);
+	}
 
-    return normalizedPath;
+	var normalizedPath = baseComponents.join("/");
+
+	if (normalizedPath.substr(normalizedPath.length - 3) !== ".js") {
+		if (isCoffeeScript(basePath)) {
+			normalizedPath += ".coffee";
+		}
+		else {
+			normalizedPath += ".js";
+		}
+	}
+
+	return normalizedPath;
 
 
-    function isFullPath(path) {
-        var unixFullPath = (path.charAt(0) === "/");
-        var windowsFullPath = (path.indexOf(":") !== -1);
+	function isFullPath(path) {
+		var unixFullPath = (path.charAt(0) === "/");
+		var windowsFullPath = (path.indexOf(":") !== -1);
 
-        return unixFullPath || windowsFullPath;
-    }
+		return unixFullPath || windowsFullPath;
+	}
 
-    function isNpmModulePath(path) {
-      return path.charAt(0) !== ".";
-    }
+	function isNpmModulePath(path) {
+		return path.charAt(0) !== ".";
+	}
 }
 
 function isCoffeeScript(filepath) {
-  return filepath.slice(-7) === '.coffee';
+	return filepath.slice(-7) === '.coffee';
 }
--- test/commonjs_bridge.spec.js
+++ test/commonjs_bridge.spec.js
@@ -1,156 +1,156 @@
-describe('client', function() {
+describe('client', function () {
 
-  describe('require high level tests', function () {
+	describe('require high level tests', function () {
 
-    beforeEach(function(){
-      window.__cjs_module__ = {};
-      window.__cjs_modules_root__ = '/root';
-    });
+		beforeEach(function () {
+			window.__cjs_module__ = {};
+			window.__cjs_modules_root__ = '/root';
+		});
 
-    describe('resolve from a file', function () {
+		describe('resolve from a file', function () {
 
-      beforeEach(function () {
-        window.__cjs_module__['/folder/foo.js'] = function(require, module, exports) {
-          exports.foo = true;
-        };
-      });
+			beforeEach(function () {
+				window.__cjs_module__['/folder/foo.js'] = function (require, module, exports) {
+					exports.foo = true;
+				};
+			});
 
-      it('should resolve absolute dependencies', function () {
-        expect(require('/folder/bar.js', '/folder/foo.js').foo).toBeTruthy();
-        //TODO: check how this works in node: expect(require('/folder/bar.js', '/folder/foo').foo).toBeTruthy();
-      });
+			it('should resolve absolute dependencies', function () {
+				expect(require('/folder/bar.js', '/folder/foo.js').foo).toBeTruthy();
+				//TODO: check how this works in node: expect(require('/folder/bar.js', '/folder/foo').foo).toBeTruthy();
+			});
 
-      it('should resolve relative dependencies', function () {
-        expect(require('/folder/bar.js', './foo').foo).toBeTruthy();
-        expect(require('/folder/bar.js', './foo.js').foo).toBeTruthy();
-        expect(require('/folder/bar.js', './../folder/foo.js').foo).toBeTruthy();
-      });
-    });
+			it('should resolve relative dependencies', function () {
+				expect(require('/folder/bar.js', './foo').foo).toBeTruthy();
+				expect(require('/folder/bar.js', './foo.js').foo).toBeTruthy();
+				expect(require('/folder/bar.js', './../folder/foo.js').foo).toBeTruthy();
+			});
+		});
 
-    describe('resolve from node_modules', function () {
+		describe('resolve from node_modules', function () {
 
-      beforeEach(function () {
-        window.__cjs_modules_root__ = '/root/node_modules';
-        window.__cjs_module__['/root/node_modules/mymodule/foo.js'] = function(require, module, exports) {
-          exports.foo = true;
-        };
-      });
+			beforeEach(function () {
+				window.__cjs_modules_root__ = '/root/node_modules';
+				window.__cjs_module__['/root/node_modules/mymodule/foo.js'] = function (require, module, exports) {
+					exports.foo = true;
+				};
+			});
 
-      it('should resolve node_modules dependencies', function () {
-        expect(require('/folder/bar.js', 'mymodule/foo').foo).toBeTruthy();
-        expect(require('/folder/bar.js', 'mymodule/foo.js').foo).toBeTruthy();
-      });
+			it('should resolve node_modules dependencies', function () {
+				expect(require('/folder/bar.js', 'mymodule/foo').foo).toBeTruthy();
+				expect(require('/folder/bar.js', 'mymodule/foo.js').foo).toBeTruthy();
+			});
 
-    });
+		});
 
-    describe('resolve - corner cases', function () {
+		describe('resolve - corner cases', function () {
 
-      it('should throw error when a required module does not exist', function () {
-        expect(function () {
-          require('/file.js', './bar')
-        }).toThrow(new Error("Could not find module '/bar.js' from '/file.js'"));
-      });
+			it('should throw error when a required module does not exist', function () {
+				expect(function () {
+					require('/file.js', './bar')
+				}).toThrow(new Error("Could not find module '/bar.js' from '/file.js'"));
+			});
 
-      it('should correctly resolve modules with circular dependencies issue #6', function(){
+			it('should correctly resolve modules with circular dependencies issue #6', function () {
 
-        window.__cjs_module__['/foo.js'] = function(require, module, exports) {
-          var bar = require('./bar');
-          exports.txt = 'foo';
-          exports.getText = function() {
-            return 'foo ' + bar.txt;
-          };
-        };
+				window.__cjs_module__['/foo.js'] = function (require, module, exports) {
+					var bar = require('./bar');
+					exports.txt = 'foo';
+					exports.getText = function () {
+						return 'foo ' + bar.txt;
+					};
+				};
 
-        window.__cjs_module__['/bar.js'] = function(require, module, exports) {
-          var foo = require('./foo');
-          exports.txt = 'bar';
-          exports.getText = function() {
-            return 'bar ' + foo.txt;
-          }
-        };
+				window.__cjs_module__['/bar.js'] = function (require, module, exports) {
+					var foo = require('./foo');
+					exports.txt = 'bar';
+					exports.getText = function () {
+						return 'bar ' + foo.txt;
+					}
+				};
 
-        expect(require('/app.js', './foo').getText()).toEqual('foo bar');
-        expect(require('/app.js', './bar').getText()).toEqual('bar foo');
-      });
-    });
-  });
+				expect(require('/app.js', './foo').getText()).toEqual('foo bar');
+				expect(require('/app.js', './bar').getText()).toEqual('bar foo');
+			});
+		});
+	});
 
-  describe('resolving of index files for both JS and CoffeeScript', function () {
+	describe('resolving of index files for both JS and CoffeeScript', function () {
 
-    it('should resolve paths like "./foo" to "./foo/Index.js" if foo.js does not exist', function () {
+		it('should resolve paths like "./foo" to "./foo/Index.js" if foo.js does not exist', function () {
 
-      window.__cjs_module__['/foo/Index.js'] = function(require, module, exports) {
-        exports.message = 'hello from Index.js';
-      };
+			window.__cjs_module__['/foo/Index.js'] = function (require, module, exports) {
+				exports.message = 'hello from Index.js';
+			};
 
-      expect(require('/', './foo').message).toEqual('hello from Index.js');
-    });
+			expect(require('/', './foo').message).toEqual('hello from Index.js');
+		});
 
-    it('should resolve paths like "./foo" to "./foo/index.js" if foo.js does not exist', function () {
+		it('should resolve paths like "./foo" to "./foo/index.js" if foo.js does not exist', function () {
 
-      window.__cjs_module__['/foo/index.js'] = function(require, module, exports) {
-        exports.message = 'hello from index.js';
-      };
+			window.__cjs_module__['/foo/index.js'] = function (require, module, exports) {
+				exports.message = 'hello from index.js';
+			};
 
-      expect(require('/', './foo').message).toEqual('hello from index.js');
-    });
+			expect(require('/', './foo').message).toEqual('hello from index.js');
+		});
 
-    it('should resolve paths like "./foo" to "./foo/Index.coffee" if foo.js / foo.coffee does not exist', function () {
+		it('should resolve paths like "./foo" to "./foo/Index.coffee" if foo.js / foo.coffee does not exist', function () {
 
-      window.__cjs_module__['/foo/Index.coffee'] = function(require, module, exports) {
-        exports.message = 'hello from Index.coffee';
-      };
+			window.__cjs_module__['/foo/Index.coffee'] = function (require, module, exports) {
+				exports.message = 'hello from Index.coffee';
+			};
 
-      expect(require('/', './foo').message).toEqual('hello from Index.coffee');
-    });
+			expect(require('/', './foo').message).toEqual('hello from Index.coffee');
+		});
 
-    it('should resolve paths like "./foo" to "./foo/index.coffee" if foo.js / foo.coffee does not exist', function () {
+		it('should resolve paths like "./foo" to "./foo/index.coffee" if foo.js / foo.coffee does not exist', function () {
 
-      window.__cjs_module__['/foo/index.coffee'] = function(require, module, exports) {
-        exports.message = 'hello from index.coffee';
-      };
+			window.__cjs_module__['/foo/index.coffee'] = function (require, module, exports) {
+				exports.message = 'hello from index.coffee';
+			};
 
-      expect(require('/', './foo').message).toEqual('hello from index.coffee');
-    });
+			expect(require('/', './foo').message).toEqual('hello from index.coffee');
+		});
 
-    it('should resolve JS files first in paths like "./foo" if foo.js / foo.coffee does not exist', function () {
+		it('should resolve JS files first in paths like "./foo" if foo.js / foo.coffee does not exist', function () {
 
-      window.__cjs_module__['/foo/index.js'] = function(require, module, exports) {
-        exports.message = 'hello from index.js';
-      };
+			window.__cjs_module__['/foo/index.js'] = function (require, module, exports) {
+				exports.message = 'hello from index.js';
+			};
 
-      window.__cjs_module__['/foo/index.coffee'] = function(require, module, exports) {
-        exports.message = 'hello from index.coffee';
-      };
+			window.__cjs_module__['/foo/index.coffee'] = function (require, module, exports) {
+				exports.message = 'hello from index.coffee';
+			};
 
-      expect(require('/', './foo').message).toEqual('hello from index.js');
-    });
+			expect(require('/', './foo').message).toEqual('hello from index.js');
+		});
 
-  });
+	});
 
-	describe('path resolving and normalization', function(){
+	describe('path resolving and normalization', function () {
 
-		it('should properly resolve full paths', function(){
+		it('should properly resolve full paths', function () {
 			expect(normalizePath('/base/foo.js', '/home/bar.js')).toEqual('/home/bar.js');
 		});
 
-		it('should properly resolve relative paths starting with .', function(){
+		it('should properly resolve relative paths starting with .', function () {
 			expect(normalizePath('/base/foo.js', './bar.js')).toEqual('/base/bar.js');
 		});
 
-		it('should properly resolve relative paths starting with ..', function(){
+		it('should properly resolve relative paths starting with ..', function () {
 			expect(normalizePath('/base/sub/foo.js', '../bar.js')).toEqual('/base/bar.js');
 		});
 
-		it('should add .js suffix if necessary', function(){
+		it('should add .js suffix if necessary', function () {
 			expect(normalizePath('/foo.js', './bar')).toEqual('/bar.js');
 		});
 
-		it('should properly handle .. and . inside paths', function(){
+		it('should properly handle .. and . inside paths', function () {
 			expect(normalizePath('/base/sub/foo.js', './other/../../sub2/./bar.js')).toEqual('/base/sub2/bar.js');
 		});
 
-		it('should resolve paths without qualifiers as relative to a defined root', function(){
+		it('should resolve paths without qualifiers as relative to a defined root', function () {
 			expect(normalizePath('/base/foo.js', 'bar', '/my/root')).toEqual('/my/root/bar.js');
 		});
 
